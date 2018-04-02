5f007df659e094180b345919418ff566e66dd1ec
Mohamed Shaban <Mohamed@Mohameds-MacBook-Pro.local>
2018-04-02 06:46:20 -0700

    README

---
+++ README.md
@@ -0,0 +1,8 @@
+# The Psychic Game
+
+A simple letter guessing game written in JavaScript.
+
+Choose a letter from A - Z and test your psycic ability to see if you can read a computers mind!
+
+
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,52 +10,52 @@ var guessedLetters = [];
 var computerGuess = computer[Math.floor(Math.random() * computer.length)];
 
 var updateGuessesLeft = function () {
-        document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
-        };
+    document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+};
 
-    var updateLetterToGuess = function () {
-        this.letterToGuess = this.computer[Math.floor(Math.random() * this.computer.length)];
-        };
-    var updateGuessesSoFar = function () {
-        document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
-        };
+var updateLetterToGuess = function () {
+    letterToGuess = computer[Math.floor(Math.random() * computer.length)];
+};
+var updateGuessesSoFar = function () {
+    document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+};
 
-    
-    
-    document.onkeyup = function (event) {
-            guessesLeft--;
-            var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        
-            guessedLetters.push(userGuess);
-            updateGuessesLeft();
-            updateGuessesSoFar();
-        
-            if (guessesLeft > 0) {
-                if (userGuess == letterToGuess) {
-                    wins++;
-                    document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                    reset();
-                }
-            } else if (guessesLeft == 0) {
-                losses++;
-                document.querySelector('#losses').innerHTML = "Losses: " + losses;
-                reset();
-            }
-            };
-        
 
-            
-    var reset = function () {
-        totalGuesses = 9;
-        guessesLeft = 9;
-        guessedLetters = [];
 
-        updateLetterToGuess();
-        updateGuessesLeft();
-        updateGuessesSoFar();
+document.onkeyup = function (event) {
+    guessesLeft--;
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    guessedLetters.push(userGuess);
+    updateGuessesLeft();
+    updateGuessesSoFar();
+
+    if (guessesLeft > 0) {
+        if (userGuess == letterToGuess) {
+            wins++;
+            document.querySelector('#wins').innerHTML = "Wins: " + wins;
+            reset();
         }
+    } else if (guessesLeft == 0) {
+        losses++;
+        document.querySelector('#losses').innerHTML = "Losses: " + losses;
+        reset();
+    }
+};
 
-        updateLetterToGuess();
-        updateGuessesLeft();
+
+
+var reset = function () {
+    totalGuesses = 9;
+    guessesLeft = 9;
+    guessedLetters = [];
+
+    updateLetterToGuess();
+    updateGuessesLeft();
+    updateGuessesSoFar();
+}
+
+updateLetterToGuess();
+updateGuessesLeft();
 
 
